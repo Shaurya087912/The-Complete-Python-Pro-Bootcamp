218e1ed8827825d31dd0608e583f7e2cc353ee32
Shaurya087912 <shauryatyagi0987@gmail.com>
2025-05-24 00:36:54 +0530

    Delete day1 directory

--- day1/DAY 1 (BAND NAME GENRATOR).py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the band name genrator")
-name_of_the_city=input("enter the name of the city you grew up in\n")
-pet_name=input("whats your pet name\n")
-print(name_of_the_city+" "+pet_name)
