2e365d074b14d11d64a689ac4ce27df4febf3792
Shaurya087912 <shauryatyagi0987@gmail.com>
2025-05-24 00:36:03 +0530

    Add files via upload

---
+++ day1/DAY 1 (BAND NAME GENRATOR).py
@@ -0,0 +1,4 @@
+print("Welcome to the band name genrator")
+name_of_the_city=input("enter the name of the city you grew up in\n")
+pet_name=input("whats your pet name\n")
+print(name_of_the_city+" "+pet_name)
